a3f5796aa2d2156db20ef068ca104599dbd550df
rajneesh <rajneesh4770@gmail.com>
2022-07-06 13:21:55 +0530

    1

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ import style from '../styles/Home.module.css';
 function index() {
   return (
     <div className={style.body}>
-      <h1>hello</h1>
+      <h1 className={style.h}>hello</h1>
       {/* Crousel component of Home page */}
       <HomeCrousel />
 
