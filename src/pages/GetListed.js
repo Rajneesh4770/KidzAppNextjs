fc392bddcd96338bce70151402d2f2c0b91824c0
Rajneesh4770 <rajneesh4770@gmail.com>
2022-07-28 18:47:43 +0530

    22

--- src/pages/GetListed.js
+++ src/pages/GetListed.js
@@ -25,7 +25,6 @@ function Getintouch() {
                   fill out the form below and you'll hear back from us very
                   soon.{" "}
                 </h5>
-                
               </div>
             </div>
           </div>
