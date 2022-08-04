b9d59a575d4baf2937d4727b214a32580565acaa
Arjunv17 <arjunverma870@gmail.com>
2022-08-04 12:54:31 +0530

    booking2 page design

--- src/pages/BlogPages/Blog1.js
+++ src/pages/BlogPages/Blog1.js
@@ -2,106 +2,108 @@ import React from 'react'
 import style from "../../styles/BlogPages/BlogPage1.module.css"
 const Blog1 = () => {
 
- 
+
   const data = [
     {
       id: 1,
       mainHeading: "Bounce over to Bounce (excuse the pun) to enjoy their Summer Offer on their best-selling ticket: the 2-Hour Superpass.",
       detail: "How to spend your cashback - top picks",
-      explanation:"The offer is valid until August 31 and at all Bounce locations in Dubai & Abu Dhabi. It includes a FREE large pizza with the purchase of the 2-hour Superpass for just AED 120! ",
-      cardImg:"",
-      location:"BOUNCE",
-      workingHours:"Sun-Thu 10:00 to 23:00, Fri-Sat until 00:00",
-      prices:"1 Child : AED 120",
+      explanation: "The offer is valid until August 31 and at all Bounce locations in Dubai & Abu Dhabi. It includes a FREE large pizza with the purchase of the 2-hour Superpass for just AED 120! ",
+      cardImg: "",
+      location: "BOUNCE",
+      workingHours: "Sun-Thu 10:00 to 23:00, Fri-Sat until 00:00",
+      prices: "1 Child : AED 120",
     },
     {
       id: 2,
       mainHeading: "Air Maniax is the ultimate fun-filled destination that has something for everyone!",
       detail: "How to spend your cashback - top picks",
-      explanation:"The venue is over 23,000 square feet and offers 6 interactive zones, so you can have a laugh at the inflatables arena, compete against each other in the multi-level warrior assault arena, experience what it's like to fly as you zip-line, navigate through a maze of lasers, or have some fun in the dedicated toddler area. ",
-      text:"Enjoy up to 20% OFF Entry Tickets at Air Maniax Dubai & Abu Dhabi!",
-      cardImg:"",
-      location:"Air Maniax, Al Quoz",
-      workingHours:"Mon-Fri 9:00 to 22:00, Sat-Sun from 10:00",
-      prices:"1hr General Access : AED 85 ",
+      explanation: "The venue is over 23,000 square feet and offers 6 interactive zones, so you can have a laugh at the inflatables arena, compete against each other in the multi-level warrior assault arena, experience what it's like to fly as you zip-line, navigate through a maze of lasers, or have some fun in the dedicated toddler area. ",
+      text: "Enjoy up to 20% OFF Entry Tickets at Air Maniax Dubai & Abu Dhabi!",
+      cardImg: "",
+      location: "Air Maniax, Al Quoz",
+      workingHours: "Mon-Fri 9:00 to 22:00, Sat-Sun from 10:00",
+      prices: "1hr General Access : AED 85 ",
     },
     {
       id: 1,
       mainHeading: "",
       detail: "How to spend your cashback - top picks",
-      explanation:"",
-      cardImg:"",
-      location:"",
-      workingHours:"",
-      prices:"",
+      explanation: "",
+      cardImg: "",
+      location: "",
+      workingHours: "",
+      prices: "",
     },
     {
       id: 1,
       mainHeading: "",
       detail: "How to spend your cashback - top picks",
-      explanation:"",
-      cardImg:"",
-      location:"",
-      workingHours:"",
-      prices:"",
+      explanation: "",
+      cardImg: "",
+      location: "",
+      workingHours: "",
+      prices: "",
     },
   ];
 
   return (
     <>
-    <section className={style.blog1}>
-<div className={style.searchAndBanner}>
-<div className={`container ${style.container}`}>
-          <div className={`row `}>
-            <div className={`col-md-12`}>
-              <div className={`searchButton d-flex`}>
-                <input
-                  type="text"
-                  name=""
-                  className={`form-control ${style.searchInput}`}
-                  placeholder="🔍 Type here to search video"
-                />
-                <button className={`btn ${style.searchBtn}`}>Search</button>
+      <section className={style.blog1}>
+        <div className={style.searchAndBanner}>
+          <div className={`container ${style.container}`}>
+            <div className={`row `}>
+              <div className={`col-md-12`}>
+                <div className={`searchButton d-flex`}>
+                  <input
+                    type="text"
+                    name=""
+                    className={`form-control ${style.searchInput}`}
+                    placeholder="🔍 Type here to search video"
+                  />
+                  <button className={`btn ${style.searchBtn}`}>Search</button>
+                </div>
               </div>
             </div>
           </div>
+
         </div>
-       
-</div>
-<div className="container mainContentSection">
-        <button className={`btn px-0 my-3 ${style.goBackBtn}`}>
-        <i class="fa-solid fa-arrow-left"></i>     Go Back
-        </button>
+        <div className="container mainContentSection">
+          <button className={`btn px-0 my-3 ${style.goBackBtn}`}>
+            <i class="fa-solid fa-arrow-left"></i>     Go Back
+          </button>
           <img className={style.mainImage} src="https://images.kidzapp.com/media/blog/cover_images/6e423d51-a9ea-4400-a5fe-ac3189fda6e8/top-action-packed-activities-in-dubai-and-abu-dhabi.jpg" alt="" />
 
-<div className={`my-4 ${style.mainText}`}>
-<h1>
-Top Action-Packed Activities in Dubai and Abu Dhabi
-</h1>
-<div className="row mt-4">
-  <div className={`col-md-8 d-flex ${style.leftContent}`}>
-<h6><i class="fa-solid fa-calendar"></i> July 28, 2022</h6>
-<h6><i class="fa-solid fa-user"></i> Posted by: Kidzapp Team</h6>
-<h6><i class="fa-solid fa-grip-lines"></i> Kidz Activities</h6>
-  </div>
-  <div className={`col-md-4 ${style.textSearchButton}`}>
-    <button className={`btn btn-primary`}><i class="fa-solid fa-share"></i> Share</button>
-  </div>
-</div>
-<p>Are you and your family looking to bond over some serious action? Well, keep reading along because we've gathered a number of activities in Dubai and Abu Dhabi that will guarantee you a fun-filled and memorable adventure with the family!</p>
-</div>
-<div className={style.elementorDivider}>
-  <span className={style.elementorDividerSeparator}></span>
-</div>
+          <div className={`my-4 ${style.mainText}`}>
+            <h1>
+              Top Action-Packed Activities in Dubai and Abu Dhabi
+            </h1>
+            <div className="row mt-4">
+              <div className={`col-md-8 d-flex ${style.leftContent}`}>
+                <h6><i class="fa-solid fa-calendar"></i> July 28, 2022</h6>
+                <h6><i class="fa-solid fa-user"></i> Posted by: Kidzapp Team</h6>
+                <h6><i class="fa-solid fa-grip-lines"></i> Kidz Activities</h6>
+              </div>
+              <div className={`col-md-4 ${style.textSearchButton}`}>
+                <button className={`btn btn-primary`}><i class="fa-solid fa-share"></i> Share</button>
+              </div>
+            </div>
+            <p>Are you and your family looking to bond over some serious action? Well, keep reading along because we've gathered a number of activities in Dubai and Abu Dhabi that will guarantee you a fun-filled and memorable adventure with the family!</p>
+          </div>
+          <div className={style.elementorDivider}>
+            <span className={style.elementorDividerSeparator}></span>
+          </div>
 
 
         </div>
-    </section>
-    <section className="mapCards">
+      </section>
+      <section className="mapCards">
 
-    </section>
+      </section>
     </>
   )
 }
 
 export default Blog1
+
+
--- src/pages/HomeHandpickedBooking/Booking2.js
+++ src/pages/HomeHandpickedBooking/Booking2.js
@@ -2,8 +2,20 @@ import style from "../../styles/Homehandpickedbooking/Booking.module.css";
 import Router from "next/router";
 import Link from "next/link";
 import { Button } from "@mui/material";
-import DateRangeIcon from "@mui/icons-material/DateRange";
+import AccessTimeIcon from "@mui/icons-material/AccessTime";
+import StarBorderPurple500Icon from "@mui/icons-material/StarBorderPurple500";
+import LocationOnIcon from "@mui/icons-material/LocationOn";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import WhatsAppIcon from "@mui/icons-material/WhatsApp";
+import Slider from "react-slick";
 const Booking = () => {
+  const Setting = {
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrow: false,
+  };
 
   return (
     <>
@@ -11,76 +23,226 @@ const Booking = () => {
         <div className="container">
           <div className="row">
             <div className="col-lg-12">
-              <Button
-                variant="contained"
-                className={style.backbutton}
-                onClick={() => Router.back()}
-              >
-                Go Back &nbsp;{" "}
-                <span className={style.backicon}>
-                  <i class="fa-solid fa-angles-left"></i>
-                </span>
-              </Button>
-              <br />
-
               {/* image section column */}
               <div className="row">
-                <div className="col-lg-8 col-sm-12">
-                  <img
-                    className={style.leftimg1}
-                    src="https://testimages.kidzapp.com/media/CACHE/images/venues/b00dc740-c214-4528-8b6b-025fe2942656/dubai-miracle-garden.jpg"
-                  />
+                <div className="col-lg-6 col-sm-12">
+                  <Slider {...Setting}>
+                    <img
+                      className={style.leftimg1}
+                      src="https://testimages.kidzapp.com/media/CACHE/images/venues/b00dc740-c214-4528-8b6b-025fe2942656/dubai-miracle-garden.jpg"
+                    />
+                    <img
+                      className={style.leftimg1}
+                      src="https://testimages.kidzapp.com/media/CACHE/images/venues/b00dc740-c214-4528-8b6b-025fe2942656/dubai-miracle-garden.jpg"
+                    />
+                  </Slider>
                   {/* heading and button section row */}
                   <div className="row">
-                    <div className="col-6">
-                      <h1>Dubai Miracle Garden</h1>
+                    <div className="col-12">
+                      <div className={style.blog_title}>
+                        <h2>Special Offer: KidZania Dubai</h2>
+                      </div>
+                      <p>Theme Parks, Fun & Play</p>
+                      <div className={style.blog_btn}>
+                        <span
+                          className={`${style.blogSpan} ${style.blog_rating}`}
+                        >
+                          <i class="fa-solid fa-star"></i>&nbsp; 4.5
+                        </span>
+                        <span className={`${style.blogSpan} ${style.blog_age}`}>
+                          <i class="fa-solid fa-calendar-days"></i> &nbsp; Ages4
+                          - Adults
+                        </span>
+                        <span className={`${style.blogSpan} ${style.blog_cat}`}>
+                          <i class="fa-solid fa-child"></i> &nbsp; Kids Only
+                        </span>
+                      </div>
                     </div>
-                    
                   </div>
                   {/* second row in col-6 */}
-                  <div className="row mt-4">
-                    <div className="col-6">
-                      <div className="row">
-                        <div className="col-6">
-                          <Button
-                            className={style.cardbutton2}
-                            variant="contained"
+                  <div className="row">
+                    <div className="col-lg-12">
+                      <div className="location pt-3 pb-3">
+                        <span>
+                          <i class="fa-solid fa-location-dot"></i>
+                        </span>
+                        &nbsp;
+                        <span>
+                          The Dubai Mall, Downtown, Downtown / Business Bay{" "}
+                        </span>
+                      </div>
+                      <div className="time pb-3">
+                        <span>
+                          <i class="fa-solid fa-clock"></i>
+                        </span>
+                        &nbsp;
+                        <span>Daily 10:00 to 22:00</span>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="row">
+                    <div className="col-lg-12">
+                      <div class="accordion" id="accordionExample">
+                        <div class="accordion-item">
+                          <h2 class="accordion-header" id="headingOne">
+                            <button
+                              class="accordion-button"
+                              type="button"
+                              data-bs-toggle="collapse"
+                              data-bs-target="#collapseOne"
+                              aria-expanded="true"
+                              aria-controls="collapseOne"
+                            >
+                              Description
+                            </button>
+                          </h2>
+                          <div
+                            id="collapseOne"
+                            class="accordion-collapse collapse show"
+                            aria-labelledby="headingOne"
+                            data-bs-parent="#accordionExample"
                           >
-                            Outdoor & Nature
-                          </Button>
+                            <div class="accordion-body">
+                              Spend the day at a temperature controlled indoor aqua park, complete with eight slide beams, waterfall buckets, forest and more for your little splashers. Your boys and girls are sure to love it, and it's a fantastic spot for you and your kiddos to play and stay cool all year round.
+                            </div>
+                          </div>
                         </div>
-                        <div className="col-6 mt-1">
-                          <div className={style.agetext}>
-                            {" "}
-                            Age:- &nbsp; 0-16
+                        <div class="accordion-item">
+                          <h2 class="accordion-header" id="headingTwo">
+                            <button
+                              class="accordion-button collapsed"
+                              type="button"
+                              data-bs-toggle="collapse"
+                              data-bs-target="#collapseTwo"
+                              aria-expanded="false"
+                              aria-controls="collapseTwo"
+                            >
+                              Information
+                            </button>
+                          </h2>
+                          <div
+                            id="collapseTwo"
+                            class="accordion-collapse collapse"
+                            aria-labelledby="headingTwo"
+                            data-bs-parent="#accordionExample"
+                          >
+                            <div class="accordion-body">
+                             <p>- The entry ticket allows you access for up to 2.5 hours max.</p> 
+                             <p>- Kids under 5 need to be accompanied by an adult.</p> 
+                             <p>- This offer is not valid on public holidays.</p> 
+                            </div>
                           </div>
                         </div>
                       </div>
                     </div>
                   </div>
 
+                
                   <div className="row mt-5">
                     <div className="col-12">
-                      <div className={style.priceoffer}>
-                        <center>
-                          <h4 className="pt-2">
-                            Dubai Miracle Garden - Prices and Offers
+                      <div className={style.price_offder}>
+                        <div className={style.price_title}>
+                          <h4 className={style.price_txt}>
+                            Rating and Reviews
                           </h4>
-                        </center>
-                        <center>
-                          <del>
-                            <h6 className={style.delprice}>AED 10</h6>
-                          </del>
-                        </center>
-                        <center>
-                          <h6 className={style.price}>AED 9</h6>
-                        </center>
+                        </div>
+                        <div className="row mt-5">
+                          <div className="col-4">
+                            <div className={style.blog_title}>
+                              <p>5.0</p>
+                            </div>
+                            <div className={style.blog_star}>
+                              <div>
+                                <i class="fa-solid fa-star"></i>
+                                <i class="fa-solid fa-star"></i>
+                                <i class="fa-solid fa-star"></i>
+                                <i class="fa-solid fa-star"></i>
+                              </div>
+
+                              <p>4,2556435</p>
+                            </div>
+                          </div>
+                          <div className="col-8">
+                            <div class="progress mb-3">
+                              <div
+                                class="progress-bar"
+                                role="progressbar"
+                                aria-label="Basic example"
+                                style={{
+                                  width: "5%",
+                                  backgroundColor: "#f1c71a",
+                                }}
+                                aria-valuenow="0"
+                                aria-valuemin="0"
+                                aria-valuemax="100"
+                              ></div>
+                            </div>
+                            <div class="progress mb-3">
+                              <div
+                                class="progress-bar"
+                                role="progressbar"
+                                aria-label="Basic example"
+                                style={{
+                                  width: "25%",
+                                  backgroundColor: "#f1c71a",
+                                }}
+                                aria-valuenow="25"
+                                aria-valuemin="0"
+                                aria-valuemax="100"
+                              ></div>
+                            </div>
+                            <div class="progress mb-3">
+                              <div
+                                class="progress-bar"
+                                role="progressbar"
+                                aria-label="Basic example"
+                                style={{
+                                  width: "50%",
+                                  backgroundColor: "#f1c71a",
+                                }}
+                                aria-valuenow="50"
+                                aria-valuemin="0"
+                                aria-valuemax="100"
+                              ></div>
+                            </div>
+                            <div class="progress mb-3">
+                              <div
+                                class="progress-bar"
+                                role="progressbar"
+                                aria-label="Basic example"
+                                style={{
+                                  width: "75%",
+                                  backgroundColor: "#f1c71a",
+                                }}
+                                aria-valuenow="75"
+                                aria-valuemin="0"
+                                aria-valuemax="100"
+                              ></div>
+                            </div>
+                            <div class="progress mb-3">
+                              <div
+                                class="progress-bar"
+                                role="progressbar"
+                                aria-label="Basic example"
+                                style={{
+                                  width: "100%",
+                                  backgroundColor: "#f1c71a",
+                                }}
+                                aria-valuenow="100"
+                                aria-valuemin="0"
+                                aria-valuemax="100"
+                              ></div>
+                            </div>
+                          </div>
+                        </div>
                       </div>
                     </div>
                   </div>
+
+                  
                 </div>
-                {/* right cards */}
-                <div className="col-lg-4 col-sm-12">
+
+                <div className="col-lg-6 col-sm-12">
                   <div className={style.rightsection}>
                     <div
                       className={`row pt-3 container ${style.rightsectiondiv}`}
@@ -92,7 +254,7 @@ const Booking = () => {
                       </div>
                       <div className="col-6 ">
                         <button className={style.schedulebutton}>
-                          <DateRangeIcon />
+                          {/* <DateRangeIcon /> */}
                           view schedule
                         </button>
                       </div>
@@ -109,7 +271,7 @@ const Booking = () => {
                               id="example"
                               class="form-control"
                             />
-                            
+
                           </div>
                         </div>
                       </div>
@@ -180,6 +342,7 @@ const Booking = () => {
           </div>
         </div>
       </section>
+
     </>
   );
 };
