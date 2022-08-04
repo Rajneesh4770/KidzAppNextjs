d6299786cd9dc24c5ef6bbae9508e23864fdece9
Rajneesh4770 <rajneesh4770@gmail.com>
2022-08-04 18:58:50 +0530

    08

--- src/pages/Counter.js
+++ src/pages/Counter.js
@@ -1,28 +1,28 @@
-// import React from 'react'
-// import { useSelector, useDispatch } from 'react-redux'
-// import { decrement, increment } from '../Redux/index'
+import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { decrement, increment } from '../Redux/index'
 
-// export default function Counter() {
-//   const count = useSelector((state) => state.counter.value)
-//   const dispatch = useDispatch()
+export default function Counter() {
+  const count = useSelector((state) => state.counter.value)
+  const dispatch = useDispatch()
 
-//   return (
-//     <div>
-//       <div style={{margin:'200px'}}>
-//         <button
-//           aria-label="Increment value"
-//           onClick={() => dispatch(increment())}
-//         >
-//           Incrementnoxde 
-//         </button>
-//         <span>{count}</span>
-//         <button
-//           aria-label="Decrement value"
-//           onClick={() => dispatch(decrement())}
-//         >
-//           Decrement
-//         </button>
-//       </div>
-//     </div>
-//   )
-// }
+  return (
+    <div>
+      <div style={{margin:'200px'}}>
+        <button
+          aria-label="Increment value"
+          onClick={() => dispatch(increment())}
+        >
+          Incrementnoxde 
+        </button>
+        <span>{count}</span>
+        <button
+          aria-label="Decrement value"
+          onClick={() => dispatch(decrement())}
+        >
+          Decrement
+        </button>
+      </div>
+    </div>
+  )
+}
--- src/pages/find2.js
+++ src/pages/find2.js
@@ -1,90 +1,90 @@
-import { style } from "@mui/system";
-import React, { useState, useEffect, useTransition } from "react";
-import ReactDOM from "react-dom";
-import ReactPaginate from "react-paginate";
-import Axios from "axios";
-import { baseUrl } from "../config";
-import style1 from "../styles/FindActivities.module.css";
-import { Rating, Button } from "@mui/material";
-import Stack from "@mui/material/Stack";
-import CircularProgress from "@mui/material/CircularProgress";
-import {Router} from "next/router"
+// import { style } from "@mui/system";
+// import React, { useState, useEffect, useTransition } from "react";
+// import ReactDOM from "react-dom";
+// import ReactPaginate from "react-paginate";
+// import Axios from "axios";
+// import { baseUrl } from "../config";
+// import style1 from "../styles/FindActivities.module.css";
+// import { Rating, Button } from "@mui/material";
+// import Stack from "@mui/material/Stack";
+// import CircularProgress from "@mui/material/CircularProgress";
+// import {Router} from "next/router"
 
-function find2(props) {
-  const [loader, setLoader] = useState(false);
-  const [data, setData] = useState([]);
-  const [pageindex, setPageindex] = useState(9);
-  const [scroll, setScroll] = useState(400);
+// function find2(props) {
+//   const [loader, setLoader] = useState(false);
+//   const [data, setData] = useState([]);
+//   const [pageindex, setPageindex] = useState(9);
+//   const [scroll, setScroll] = useState(400);
 
-  const [isLoading, setLoading] = useState(false);
-  const startLoading = () => setLoading(true);
-  const stopLoading = () => setLoading(false);
+//   const [isLoading, setLoading] = useState(false);
+//   const startLoading = () => setLoading(true);
+//   const stopLoading = () => setLoading(false);
 
-  useEffect(() => {
-    Router.events.on("routeChangeStart", startLoading);
-    Router.events.on("routeChangeComplete", stopLoading);
+//   useEffect(() => {
+//     Router.events.on("routeChangeStart", startLoading);
+//     Router.events.on("routeChangeComplete", stopLoading);
 
-    return () => {
-      Router.events.off("routeChangeStart", startLoading);
-      Router.events.off("routeChangeComplete", stopLoading);
-    };
-  }, []);
+//     return () => {
+//       Router.events.off("routeChangeStart", startLoading);
+//       Router.events.off("routeChangeComplete", stopLoading);
+//     };
+//   }, []);
 
-  const pagginationHandler = (page) => {
-    const scrolling = (event) => {
-      if (window.scrollY > scroll) {
-        setPageindex(pageindex + 4);
-        setScroll(scroll + 400);
-        console.log("hello", pageindex);
-      }
-      console.log(window.scrollY, "scroll");
-    };
-    window.addEventListener("scroll", scrolling, false);
-    return () => window.removeEventListener("scroll", scrolling, false);
-  };
+//   const pagginationHandler = (page) => {
+//     const scrolling = (event) => {
+//       if (window.scrollY > scroll) {
+//         setPageindex(pageindex + 4);
+//         setScroll(scroll + 400);
+//         console.log("hello", pageindex);
+//       }
+//       console.log(window.scrollY, "scroll");
+//     };
+//     window.addEventListener("scroll", scrolling, false);
+//     return () => window.removeEventListener("scroll", scrolling, false);
+//   };
 
-  let content = null;
-  if (isLoading) content = <div>Loading...</div>;
-  else {
-    content = <div>un Loading...</div>
-    // (
-    // //   <ul>
-    // //     {props.posts.map((post) => {
-    // //       return <li key={post.id}>{post.title}</li>;
-    // //     })}
-    // //   </ul>
-    // );
-  }
+//   let content = null;
+//   if (isLoading) content = <div>Loading...</div>;
+//   else {
+//     content = <div>un Loading...</div>
+//     // (
+//     // //   <ul>
+//     // //     {props.posts.map((post) => {
+//     // //       return <li key={post.id}>{post.title}</li>;
+//     // //     })}
+//     // //   </ul>
+//     // );
+//   }
 
-  return (
-    <div className="container">
-      <h1>Posts List with Pagination in Next.js</h1>
-      <div className="posts">{content}</div>
+//   return (
+//     <div className="container">
+//       <h1>Posts List with Pagination in Next.js</h1>
+//       <div className="posts">{content}</div>
 
-      <ReactPaginate
-        initialPage={props.pageindex - 1}
-        pageCount={props.pageindex} //page count
-        marginPagesDisplayed={2}
-        pageRangeDisplayed={5}
-        onPageChange={pagginationHandler}
-      />
-    </div>
-  );
-}
+//       <ReactPaginate
+//         initialPage={props.pageindex - 1}
+//         pageCount={props.pageindex} //page count
+//         marginPagesDisplayed={2}
+//         pageRangeDisplayed={5}
+//         onPageChange={pagginationHandler}
+//       />
+//     </div>
+//   );
+// }
 
-find2.getInitialProps = async ({ query }) => {
-  const page = query.page || 1;
-  const posts = await Axios.get(
-    baseUrl + `experiences/?country_code=ae&page=1&page_size=${page}`
-  );
-  return {
-    // totalCount: posts.data._meta.totalCount,
-    // pageCount: posts.data._meta.pageCount,
-    // currentPage: posts.data._meta.currentPage,
-    // perPage: posts.data._meta.perPage,
-    // posts: posts.data.result,
-    isLoading: false,
-  };
-};
+// find2.getInitialProps = async ({ query }) => {
+//   const page = query.page || 1;
+//   const posts = await Axios.get(
+//     baseUrl + `experiences/?country_code=ae&page=1&page_size=${page}`
+//   );
+//   return {
+//     // totalCount: posts.data._meta.totalCount,
+//     // pageCount: posts.data._meta.pageCount,
+//     // currentPage: posts.data._meta.currentPage,
+//     // perPage: posts.data._meta.perPage,
+//     // posts: posts.data.result,
+//     isLoading: false,
+//   };
+// };
 
-export default find2;
+// export default find2;
