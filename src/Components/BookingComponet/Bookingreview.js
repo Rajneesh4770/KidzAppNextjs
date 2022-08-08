67d53a1cc52a2b88bd3224d2b70cca0ad28656ee
pranjal1642 <pranjalgupta1642@gmail.com>
2022-08-08 18:07:13 +0530

    hey

--- src/Components/BookingComponet/Bookingreview.js
+++ src/Components/BookingComponet/Bookingreview.js
@@ -4,34 +4,40 @@ import { Button } from '@mui/material';
 import { useState } from 'react';
 import PaymentSection from './PaymentSection';
 function Bookingreview() {
-  const [payment,setPayment]=useState(true);
+	const [payment, setPayment] = useState(true);
 	return (
 		<>
-    {payment? 
-    <>
-			<div className="col-lg-6 col-sm-12">
-				<div className={style.rightsection}>
-					<div className="row mt-4">
-						<div className="col-lg-12">
-							<div className={style.rightsectiondiv}>
-								<p className={` m-0 ${style.datetimecontent}`}>
-									Please confirm your booking detailssssss
-								</p>
-								<p></p>
+			{payment ? (
+				<>
+					<div className="col-lg-6 col-sm-12">
+						<div className={style.rightsection}>
+							<div className="row mt-4">
+								<div className="col-lg-12">
+									<div className={style.rightsectiondiv}>
+										<p className={` m-0 ${style.datetimecontent}`}>
+											Please confirm your booking details
+										</p>
+										<p></p>
+									</div>
+								</div>
+							</div>
+							<div className="row">
+								<div className="col-lg-12">
+									<Button
+										variant="contained"
+										className={style.submitbtn}
+										onClick={() => setPayment(false)}
+									>
+										Confirm and pay now
+									</Button>
+								</div>
 							</div>
 						</div>
 					</div>
-					<div className="row">
-						<div className="col-lg-12">
-							<Button variant="contained" className={style.submitbtn} onClick={()=>(setPayment(false))}>
-								Confirm and pay now
-							</Button>
-						</div>
-					</div>
-				</div>
-			</div>
-      </>: <PaymentSection/> }
-
+				</>
+			) : (
+				<PaymentSection />
+			)}
 		</>
 	);
 }
