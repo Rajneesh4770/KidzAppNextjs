f1499d443b1da11507e7528f6779f4ab6963f07d
rajneesh <rajneesh4770@gmail.com>
2022-08-20 12:29:50 +0530

    21-08

--- src/pages/Booking.js
+++ src/pages/Booking.js
@@ -42,7 +42,6 @@ const Booking = () => {
 				);
 				return (
 					<section className={style.section1} key={item.id}>
-            <h1>hiiiii rajneesh</h1>
 						<div className="container">
 							<div className="row">
 								<div className="col-lg-12">
