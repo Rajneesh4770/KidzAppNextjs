a010b6faf27ee8b2a42ca7f9c11ffb410c0ed9e4
rajneesh <rajneesh4770@gmail.com>
2022-08-20 12:14:37 +0530

    d

--- src/pages/Booking.js
+++ src/pages/Booking.js
@@ -42,6 +42,7 @@ const Booking = () => {
         );
         return (
           <section className={style.section1} key={item.id}>
+            <h1>hello welcome</h1>
             <div className="container">
               <div className="row">
                 <div className="col-lg-12">
