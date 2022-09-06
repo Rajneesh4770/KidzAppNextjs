90cb686937feb24385b5d471569410e15df9c85e
Kumari-pratigya <pratigya.sharma101@gmail.com>
2022-09-06 17:41:18 +0530

    commmit

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,91 +1,57 @@
-import Navbar from '../Components/Navbar';
-import Head from 'next/head';
-import 'bootstrap/dist/css/bootstrap.min.css'
-import Footer from '../Components/Footer';
-import '../styles/globals.css';
-import { Provider } from 'react-redux';
-import { store } from '../Redux/Store';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import Navbar from "../Components/Navbar";
+import Head from "next/head";
+import { Provider } from "react-redux";
+import { ToastContainer } from "react-toastify";
+
+import { store } from "../redux/store";
+import Footer from "../Components/Footer";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "react-toastify/dist/ReactToastify.css";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import 'animate.css';
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-	// module.exports = {
-	//   eslint: {
-	//     // Warning: This allows production builds to successfully complete even if
-	//     // your project has ESLint errors.
-	//     ignoreDuringBuilds: true,
-	//   },
-	// }
-	return (
-		<>
-			<Head>
-				{/* bootsrap cdn */}
-				<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"/>
+  return (
+    <>
+      <Head>
+        {/* font awesome icon cdn */}
+        <link
+          rel="stylesheet"
+          type="text/css"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
+        />
+        {/* recaptcha cdn */}
+      </Head>
 
-				{/* <link
-					href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
-					rel="stylesheet"
-					integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
-					crossorigin="anonymous"
-				/>
+      <Provider store={store}>
+        <Navbar />
+        <Component {...pageProps} />
 
-				
-				/>
-				 */}
+        <ToastContainer
+          position="top-right"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={true}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+        />
 
-				{/* slick slider cdn */}
+        <Footer />
 
-				{/* <link rel="preconnect" href="https://fonts.googleapis.com" />
-				<link rel="preconnect" href="https://fonts.gstatic.com"  />
-				<link
-					href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,300;0,400;0,500;0,700;1,400&display=swap"
-					rel="stylesheet"
-				/>
 
-				<link
-					rel="stylesheet"
-					type="text/css"
-					href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css"
-				/>
-				<link
-					rel="stylesheet"
-					type="text/css"
-					href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css"
-				/> */}
-
-				{/* font awesome icon cdn */}
-				<link
-					rel="stylesheet"
-					type="text/css"
-					href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
-				/>
-				{/* recaptcha cdn */}
-			</Head>
-			<Provider store={store}>
-				<Navbar />
-					<Component {...pageProps} />
-				<ToastContainer
-					position="top-right"
-					autoClose={5000}
-					hideProgressBar={false}
-					newestOnTop={false}
-					closeOnClick
-					rtl={true}
-					pauseOnFocusLoss
-					draggable
-					pauseOnHover
-				/>
-
-				<Footer />
-
-				<script
-					src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
-					integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
-					crossorigin="anonymous"
-				></script>
-			</Provider>
-		</>
-	);
-}
+        <script
+          src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+          integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+          crossOrigin="anonymous"
+        ></script>
+      </Provider>
+    </>
+  );
 
 export default MyApp;
